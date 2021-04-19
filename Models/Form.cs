6b4fa15230c6c52804853a6f2e5abde655d7be2a
Jakub Jedlička <jakub.jedla.jedlicka@gmail.com>
2021-04-20 00:36:40 +0200

    added backend functionalitie

--- Models/Form.cs
+++ Models/Form.cs
@@ -9,32 +9,65 @@ using CompareAttribute = System.Web.Mvc.CompareAttribute;
 
 namespace WebTextAnalyzer.Models
 {
+    /**
+     * number of char                                           x
+     * number of char without whitespace                        x
+     * number of char without digits                            x
+     * number of digit
+     * number of words                                          x
+     * number of sentences                                      x
+     * longest words                                            x
+     * shortest word                                            x
+     * most commons word and their number ocurencies            
+     * number of different word                                 
+     * average number of character per word                             
+     * number of syllable                                       x
+     * number of syllable per word                              
+     */
     public class Form
     {
         [DataType(DataType.MultilineText)]
         public string Input { get; set; }
+        public int NumberOfChar { get; set; }
+        public int NumberOfCharWithoutWhiteSpace { get; set; }
+        public int NumberOfCharWithoutSpaceAndDigit { get; set; }
+        public int NumberOfWords { get; set; }
+        public int NumberOfSentences { get; set; }
+        public List<string> ListOfLongestWords { get; set; }
+        public List<string> ListOfShortestWords { get; set; }
+        public int NumberOfSyllables { get; set; }
+        public Dictionary<string, int> MostCommonWords { get; set; }
+
+
         public List<int> Result { get; set; }
 
-        private TextOperation textOperations = new TextOperation();
 
         public Form()
         {
             Result = new List<int>();
+            ListOfLongestWords = new List<string>();
+            ListOfShortestWords = new List<string>();
         }
 
-        public void Compute()
+        public void ComputeStats()
         {
-            textOperations.Text = new StringBuilder(Input);
-            Result.Add(textOperations.NumberOfCharacters());
-            Result.Add(textOperations.NumberOfCharacters());
+            TextOperation textOperations = new TextOperation(Input);
+            NumberOfChar = textOperations.CountCharacters();
+            NumberOfCharWithoutWhiteSpace = textOperations.CountCharWithoutSpace();
+            NumberOfCharWithoutSpaceAndDigit = textOperations.CountCharWithoutSpaceAndDigit();
+            NumberOfWords = textOperations.CountWords();
+            NumberOfSentences = textOperations.CountSentences();
+            ListOfLongestWords = textOperations.ListOfLongestWords();
+            ListOfShortestWords = textOperations.ListOfShortestWords();
+            NumberOfSyllables = textOperations.CountSyllable();
+            MostCommonWords = textOperations.MostCommonWords();
+            Result.Add(textOperations.CountWords());
+
+
+
+
         }
 
     }
 
-
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
--- Models/TextOperation.cs
+++ Models/TextOperation.cs
@@ -2,19 +2,194 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace WebTextAnalyzer.Models
 {
+    /**
+     * number of char                                           x
+     * number of char without whitespace                        x
+     * number of char without digits                            x
+     * number of digit
+     * number of words                                          x
+     * number of sentences                                      x
+     * longest words                                            x
+     * shortest word                                            x
+     * most commons word and their number ocurencies            
+     * number of different word                                 
+     * number of character per word                             
+     * number of syllable                                       x
+     * number of syllable per word                              
+     */
+
+    
     public class TextOperation
     {
-        public StringBuilder Text { get; set; }
+        public string Text { get; private set; }
+        private string[] Words { get; set; }
 
-
-        public int NumberOfCharacters()
+        public TextOperation(string text)
         {
+            this.Text = text;
+            this.Words = Regex.Replace(text.Replace("\t", " ").Replace("\n", " "), @"([^a-zA-Z\s])", "").Split(' ');
 
+        }
+
+        public int CountCharacters()
+        {
             return Text.Length;
+        }
+        public int CountCharWithoutSpace()
+        {
+            return Text.Replace(" ", "").Replace("\t", "").Replace("\n", "").Length;
+        }
+        public int CountCharWithoutSpaceAndDigit()
+        {
+            return Regex.Replace(Text.Replace(" ", "").Replace("\t", "").Replace("\n", ""), @"[\d]", string.Empty).Length;
+        }
+
+        public int CountWords()
+        {
+            return Words.Length;
+        }
+
+        public int CountSentences()
+        {
+            char[] delimiterChars = { '.', '?', '!' };
+            string[] row = Text.Replace(" ", "").Replace("\t", "").Replace("\n", "").Split(delimiterChars);
+
+            int counter = 0;
+            for (int i = 0; i < row.Length - 2; i++)
+            {
+                if (i == 0 && Char.IsUpper(row[i][0]))
+                {
+                    counter++;
+                }
+                if (Char.IsUpper(row[i + 1][0]))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public List<string> ListOfLongestWords()
+        {
+            List<string> longestWords = new List<string>();
+            int biggestLenght = 0;
+
+            foreach (var word in Words)
+            {
+                if (word.Length > biggestLenght)
+                {
+                    biggestLenght = word.Length;
+                    longestWords.Clear();
+                    longestWords.Add(word);
+                }
+                else if (word.Length == biggestLenght && !longestWords.Contains(word))
+                {
+                    longestWords.Add(word);
+                }
+            }
+
+            return longestWords;
+        }
+
+        public List<string> ListOfShortestWords()
+        {
+            List<string> longestWords = new List<string>();
+            int biggestLenght = 0;
+
+            foreach (var word in Words)
+            {
+                if (word.Length < biggestLenght)
+                {
+                    biggestLenght = word.Length;
+                    longestWords.Clear();
+                    longestWords.Add(word);
+                }
+                else if (word.Length == biggestLenght && !longestWords.Contains(word))
+                {
+                    longestWords.Add(word);
+                }
+            }
+
+            return longestWords;
+        }
+
+        //https://stackoverflow.com/questions/61033977/how-to-determine-syllables-in-a-word-by-using-regular-expression
+        //https://codereview.stackexchange.com/questions/9972/syllable-counting-function
+        public int CountSyllable()
+        {
+            string text = Text.ToLower().Trim();
+            bool lastWasVowel = true;
+            var vowels = new[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+            int count = 0;
+
+            //a string is an IEnumerable<char>; convenient.
+            foreach (var character in text)
+            {
+                if (vowels.Contains(character))
+                {
+                    if (!lastWasVowel)
+                        count++;
+                    lastWasVowel = true;
+                }
+                else
+                    lastWasVowel = false;
+            }
+
+            if ((text.EndsWith("e") || (text.EndsWith("es") || text.EndsWith("ed")))
+                  && !text.EndsWith("le"))
+                count--;
+
+            return count;
+        }
+
+        public Dictionary<string, int> MostCommonWords()
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            Dictionary<string, int> sortedDictionary = new Dictionary<string, int>();       
+            foreach (var item in Words)
+            {
+                if (dictionary.ContainsKey(item))
+                {
+                    dictionary[item]++;
+                }
+                else
+                {
+                    dictionary.Add(item, 1);
+                }
+            }
+
+
+            sortedDictionary = dictionary.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            var count = sortedDictionary.Count();
+            dictionary.Clear();
+           // sortedDictionary = sortedDictionary.Reverse();
+            for (int i = 0; i < count && i < 10 ; i++)
+            {
+                var item = sortedDictionary.ElementAt(i);
+                dictionary.Add(item.Key, item.Value);
+            }
+                       
+            /**
+            foreach (var key in dict)
+            {
+                if (key.Value > ocurencies)
+                {
+                    ocurencies = key.Value;
+                    commonWords.Clear();
+                    commonWords.Add(key.Key);
+                }
+                else if (key.Value == ocurencies)
+                {
+                    commonWords.Add(key.Key);
+                }
+            }
+            **/
+            return dictionary;
         }
 
     }
